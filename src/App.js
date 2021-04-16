c24b44d53ef8006d8e5789b1503406daddaed5c8
michaelquintdev <michaelquintdev@gmail.com>
2021-04-16 10:40:50 -0500

    Im going to take a nap, but the API is connected

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from "axios";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  // State Declarations
+  const [films, setFilms] = useState('');
+  const [characters, setCharacters] = useState('');
+
+  // Calling API
+  useEffect(() => {
+    axios
+    .get('https://swapi.dev/api/films/')
+    .then(res => {
+      console.log(res.data.results);
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }, []);
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
